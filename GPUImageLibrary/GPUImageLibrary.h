9b29c89a858513798b3035cccd56d9f30cdacc79
mao yu <maoyu417@163.com>
2013-03-27 10:24:41 +0800

    新建静态库

---
+++ GPUImageLibrary/GPUImageLibrary.h
@@ -0,0 +1,13 @@
+//
+//  GPUImageLibrary.h
+//  GPUImageLibrary
+//
+//  Created by lixiaoyu on 13-3-25.
+//  Copyright (c) 2013年 Liu&Mao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPUImageLibrary : NSObject
+
+@end
---
+++ GPUImageLibrary/GPUImageLibrary.m
@@ -0,0 +1,13 @@
+//
+//  GPUImageLibrary.m
+//  GPUImageLibrary
+//
+//  Created by lixiaoyu on 13-3-25.
+//  Copyright (c) 2013年 Liu&Mao. All rights reserved.
+//
+
+#import "GPUImageLibrary.h"
+
+@implementation GPUImageLibrary
+
+@end
